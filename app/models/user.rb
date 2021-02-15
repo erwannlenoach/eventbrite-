d2957680a30283557f8bae7350f0e6d8cd34398f
Erwann Le Noac’h <>
2021-02-15 20:41:53 +0100

    tables & models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+	has_many :attendances  
+	has_many :events, through: :attendances
+    has_many :events, foreign_key: 'admin_id', class_name: "Event"
+    validates :name, length: { minimum: 2 }
+    validates :bio, length: { maximum: 500 }
+    validates :password, length: { in: 6..20 }
+    validates :registration_number, length: { is: 6 }
+end
