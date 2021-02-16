4c89dd454a38f668bf24531d19da7982476dc80e
Erwann Le Noac’h <>
2021-02-16 18:41:30 +0100

    operational nav bar & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/secret'
+
+  root 'events#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :events
+  resources :users
 end
