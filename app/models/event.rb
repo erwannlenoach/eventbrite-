4ebbcefafc51c4fa2d1424d5564864a5a2fa6975
Erwann Le Noac’h <>
2021-02-16 18:57:48 +0100

    update model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :admin, class_name: "User"
-    validates :start_date, presence: true,
-    validates :duration, presence: true, :numericality => { :greater_than => 0 }
-    validates :title, presence: true, length: length: 5..140
+    validates :start_date, presence: true
+    validates :duration, presence: true, numericality: { greater_than: 0 }
+    validates :title, presence: true, length: 5..140
     validates :description, presence: true, length: 20..1000
     validates :price, presence: true, inclusion: { in: 1..1000}
     validates :location, presence: true
